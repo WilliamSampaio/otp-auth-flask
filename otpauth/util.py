f3efd3627c167f76d5c4d1262f3794388a7d85ad
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-12-12 17:46:04 -0400

    extract_num_from_str

--- otpauth/util.py
+++ otpauth/util.py
@@ -11,3 +11,7 @@ def qrcode_base64(data: str) -> str:
     file_bytes = file.getvalue()
     b64 = base64.b64encode(file_bytes).decode()
     return 'data:image/png;charset=utf-8;base64,' + b64
+
+
+def extract_num_from_str(txt: str) -> str:
+    return ''.join([i for i in txt.split() if i.isdigit()])
