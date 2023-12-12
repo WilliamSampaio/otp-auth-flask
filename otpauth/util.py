78f8c602e52c94a39ab3e41fd6271e4f17d7c3ac
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-12-12 17:29:30 -0400

    database and util

---
+++ otpauth/util.py
@@ -0,0 +1,13 @@
+import base64
+import io
+
+import qrcode
+
+
+def qrcode_base64(data: str) -> str:
+    qrcode_img = qrcode.make(data)
+    file = io.BytesIO()
+    qrcode_img.save(file)
+    file_bytes = file.getvalue()
+    b64 = base64.b64encode(file_bytes).decode()
+    return 'data:image/png;charset=utf-8;base64,' + b64
