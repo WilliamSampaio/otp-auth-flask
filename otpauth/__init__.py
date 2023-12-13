d441307d3f5ebd14c1ac73ba00c306df2812d28b
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-12-13 01:34:45 -0400

    flask-session

--- otpauth/__init__.py
+++ otpauth/__init__.py
@@ -2,15 +2,27 @@ __version__ = '0.1.0'
 
 
 import pyotp
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from tinydb import Query
 
+from flask_session import Session
 from otpauth.util import extract_num_from_str, qrcode_base64
 
 from .database import get_users_tbl
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app=app)
 
 
 @app.route('/')
